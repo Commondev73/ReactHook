7d61a055a1e9d8d017a4fd5fef03d0666e04d285
- <->
2020-09-30 20:28:12 +0700

    test

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+//redux
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import allReducer from "./reducers";
+
+const middleware = [thunk];
+const store = createStore(allReducer, applyMiddleware(...middleware));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
